999aea61110c714c40970e73ac85b7ed02831649
memzuuu <91360407+memzuuu@users.noreply.github.com>
2021-09-25 20:51:03 +0800

    Add files via upload

---
+++ Practical Exam 1/3B.sql
@@ -0,0 +1,70 @@
+-- Get all employees that are hired from 2017-2018
+
+-- Creates the database
+DROP DATABASE IF EXISTS `prac_exam_db`;
+CREATE DATABASE `prac_exam_db`;
+USE `prac_exam_db`;
+
+-- Creates 'departments' table
+CREATE TABLE `departments` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `department` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- Creates 'salary' table
+CREATE TABLE `salary` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `salary` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- Creates 'employees' table
+CREATE TABLE `employees` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `date_hired` datetime NOT NULL,
+  `department_id` int(10) unsigned NOT NULL,
+  `salary_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`department_id`) REFERENCES `departments` (`id`),
+  FOREIGN KEY (`salary_id`) REFERENCES `salary` (`id`)
+);
+
+-- Inserts values into the tables
+INSERT INTO `departments` (`department`)
+	VALUES('Marketing');
+    
+INSERT INTO `departments` (`department`)
+	VALUES('Consultancy');
+
+INSERT INTO `departments` (`department`)
+	VALUES('IT');
+    
+INSERT INTO `salary` (`salary`)
+	VALUES(10000);
+    
+INSERT INTO `salary` (`salary`)
+	VALUES(15000);
+    
+INSERT INTO `salary` (`salary`)
+	VALUES(20000);
+
+INSERT INTO `employees` (`name`, `date_hired`, `department_id`, `salary_id`)
+	VALUES('Employee 1', '2017-10-07 00:00:00', 3, 1);
+
+INSERT INTO `employees` (`name`, `date_hired`, `department_id`, `salary_id`)
+	VALUES('Employee 2', '2012-10-08 00:00:00', 2, 3);
+    
+INSERT INTO `employees` (`name`, `date_hired`, `department_id`, `salary_id`)
+	VALUES('Employee 3', '2016-08-10 00:00:00', 1, 2);
+    
+INSERT INTO `employees` (`name`, `date_hired`, `department_id`, `salary_id`)
+	VALUES('Employee 4', '2018-08-10 00:00:00', 3, 2);
+    
+-- Displays all information of employeed hired from 2017-2018
+SELECT `employees`.`id`, `employees`.`name`, `employees`.`date_hired`, `departments`.`department`, `salary`.`salary`
+	FROM `employees`
+    INNER JOIN `departments` ON `employees`.`department_id` = `departments`.`id`
+    INNER JOIN `salary` ON `employees`.`salary_id` = `salary`.`id`
+    WHERE `date_hired` BETWEEN '2017-01-01 00:00:00' AND '2018-12-31 23:59:59';
